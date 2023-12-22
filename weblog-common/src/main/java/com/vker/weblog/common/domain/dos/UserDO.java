1f7996e7771017a0e4a16609cee6b01c25d720bd
vker <krismile_code@163.com>
2023-12-22 19:59:14 +0800

    登录

--- weblog-common/src/main/java/com/vker/weblog/common/domain/dos/UserDO.java
+++ weblog-common/src/main/java/com/vker/weblog/common/domain/dos/UserDO.java
@@ -23,18 +23,34 @@ import java.time.LocalDateTime;
 @TableName("t_user")
 public class UserDO {
 
+    /**
+     * 主键
+     */
     @TableId(type = IdType.AUTO)
     private Long id;
 
+    /**
+     * 用户名
+     */
     private String username;
 
+    /**
+     * 密码
+     */
     private String password;
 
+    /**
+     * 创建时间
+     */
     private LocalDateTime createTime;
 
+    /**
+     * 修改时间
+     */
     private LocalDateTime updateTime;
 
+    /**
+     * 逻辑删除
+     */
     private Boolean isDeleted;
-
-
 }
